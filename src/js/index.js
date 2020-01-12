87a8ccb83d007e53f119cc3c37973eb5b05c86ff
Cyber <Tsuchinoko.nya@gmail.com>
2020-01-12 18:53:02 +0600

    Working on carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import "./import/modules";
+
+$(document).ready(function(){
+    $('.block-carousel').slick({
+        dots: true,
+        centerMode: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.block-carousel').slick({
+        dots: true,
+        centerMode: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
